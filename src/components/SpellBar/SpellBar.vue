cf066519be5586217036eec4db97a24d6e3a736b
Th1nkK1D <witheep@gmail.com>
2020-04-10 17:36:59 +0700

    Update SpellBar and layout style

--- src/components/SpellBar/SpellBar.vue
+++ src/components/SpellBar/SpellBar.vue
@@ -1,12 +1,19 @@
 <template>
-  <div>
+  <div class="flex flex-row bg-white h-12 max-w-md m-auto rounded-full p-1 shadow-md">
     <img
       v-if="spell"
       :src="spell.icon"
       :alt="spell.name"
       role="img"
+      class="h-full rounded-full"
     >
-    {{ spell ? spell.name : 'Press space to start' }}
+    <div class="text-black flex-1 flex flex-col justify-center font-bold">
+      {{ spell ? spell.name : 'Press space to start' }}
+    </div>
+    <div
+      v-if="spell"
+      class="w-10"
+    />
   </div>
 </template>
 
