1bf08da8a57dd1922c23174d49b91d2d26b786c3
Th1nkK1D <witheep@gmail.com>
2020-05-04 20:20:23 +0700

    Remove header and update footer text

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="h-screen bg-black flex flex-col">
-    <header class="p-3 text-white text-center">
-      Invoker Playground
-    </header>
     <div class="container flex-1 mx-auto">
       <slot />
     </div>
     <footer class="p-3 text-xs text-gray-500 text-center">
-      All right reserved
+      &copy; Copyright {{ (new Date()).getFullYear() }} - <a
+        href="https://github.com/Th1nkK1D/invoker-playground"
+        target="_blank"
+        class="underline"
+      >Made with &lt;3 by Th1nkk1d</a>
     </footer>
   </div>
 </template>
